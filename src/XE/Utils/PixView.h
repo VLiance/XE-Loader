ddaaea45bf72fa23f9addf8946eed55088f8ac95
Maeiky <maeiky@gmail.com>
2021-03-22 15:51:29 -0400

    Add border

--- src/XE/Utils/PixView.h
+++ src/XE/Utils/PixView.h
@@ -32,8 +32,8 @@ struct pixel {
  bool bLButtonDown  = false;
  
 #ifdef USE_Transparent_PixView
-	#define WIN_BORDER_T 10
-	#define WIN_BORDER_L 0
+	#define WIN_BORDER_T 20
+	#define WIN_BORDER_L 5
 	#define WIN_BORDER_R WIN_BORDER_L
 	#define WIN_BORDER_B WIN_BORDER_L
 	
@@ -95,68 +95,81 @@ LRESULT CALLBACK WndProc( HWND hwnd,UINT msg,WPARAM wParam,LPARAM lParam){
 
 HWND pixView_createWindow( HINSTANCE hInstance, ContextInf* _context){
 
-
 	_context->mem_width  = _context->width+ WIN_BORDER_L + WIN_BORDER_R;
 	_context->mem_height = _context->height+ WIN_BORDER_T + WIN_BORDER_B;
-	_context->off_x = WIN_BORDER_L;
 	_context->off_y = WIN_BORDER_T;
-   
+	_context->off_x = WIN_BORDER_L;
 
-  static bool class_registred = false;
-  if(!class_registred){
-	  WNDCLASSEX wc;
-	  wc.cbClsExtra = 0;
-	  wc.cbWndExtra = 0;
-	  wc.cbSize = sizeof( WNDCLASSEX );
-	  wc.hbrBackground = CreateSolidBrush( 0 );
-	  wc.hCursor = LoadCursor( NULL, IDC_ARROW );
-	  wc.hIcon = LoadIcon( NULL, IDI_APPLICATION );
-	  wc.hIconSm = LoadIcon( NULL, IDI_APPLICATION );
-	  wc.hInstance = hInstance;
-	  wc.lpfnWndProc = WndProc;
-	  wc.lpszClassName = "pixview_class";
-	  wc.lpszMenuName = NULL;
-	  wc.style = 0;
+	static bool class_registred = false;
+	if(!class_registred){
+		WNDCLASSEX wc;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
+		wc.cbSize = sizeof( WNDCLASSEX );
+		wc.hbrBackground = CreateSolidBrush( 0 );
+		wc.hCursor = LoadCursor( NULL, IDC_ARROW );
+		wc.hIcon = LoadIcon( NULL, IDI_APPLICATION );
+		wc.hIconSm = LoadIcon( NULL, IDI_APPLICATION );
+		wc.hInstance = hInstance;
+		wc.lpfnWndProc = WndProc;
+		wc.lpszClassName = "pixview_class";
+		wc.lpszMenuName = NULL;
+		wc.style = 0;
 
-	  if ( !RegisterClassEx(&wc) ) {
-		MessageBox( NULL, "Failed to register window class.", "Error", MB_OK );
-		return 0;
-	  }
-	  class_registred = true;
-  }
-  
- //HINSTANCE hInstance = GetModuleHandle(NULL);
-  HWND hwnd = CreateWindowEx(
+		if ( !RegisterClassEx(&wc) ) {
+			MessageBox( NULL, "Failed to register window class.", "Error", MB_OK );
+			return 0;
+		}
+		class_registred = true;
+	}
+
+	//HINSTANCE hInstance = GetModuleHandle(NULL);
+	HWND hwnd = CreateWindowEx(
 		#ifdef USE_Transparent_PixView
 			WS_EX_LAYERED  | WS_EX_NOPARENTNOTIFY, //WS_EX_LAYERED, // WS_EX_APPWINDOW,
 		#else
 			WS_EX_APPWINDOW  | WS_EX_NOPARENTNOTIFY, //WS_EX_LAYERED, // WS_EX_APPWINDOW,
 		#endif
 
-    "pixview_class",
-    "pixview",
-	WS_MINIMIZEBOX | WS_SYSMENU | WS_POPUP | WS_CAPTION,
-    300, 200, _context->mem_width, _context->mem_height,
-    NULL, NULL, hInstance, NULL );
+		"pixview_class",
+		"pixview",
+		WS_MINIMIZEBOX | WS_SYSMENU | WS_POPUP | WS_CAPTION,
+		300, 200, _context->mem_width, _context->mem_height,
+		NULL, NULL, hInstance, NULL 
+	);
 
-  if ( !hwnd ) {
-    MessageBox( NULL, "Failed to create PixView", "Error", MB_OK );
-    return 0;
-  }
+	if ( !hwnd ) {
+		MessageBox( NULL, "Failed to create PixView", "Error", MB_OK );
+		return 0;
+	}
 
-  RECT rcClient, rcWindow;
-  POINT ptDiff;
+	RECT rcClient, rcWindow;
+	POINT ptDiff;
 
-  GetClientRect( hwnd, &rcClient );
-  GetWindowRect( hwnd, &rcWindow );
-  ptDiff.x = (rcWindow.right - rcWindow.left) - rcClient.right;
-  ptDiff.y = (rcWindow.bottom - rcWindow.top) - rcClient.bottom;
-  MoveWindow( hwnd, rcWindow.left, rcWindow.top, _context->width + ptDiff.x, _context->height + ptDiff.y, false);
-  
-//int _nTitleOffset = fClientResize(hWnd, _oWindow->vFrame.nWidth, _oWindow->vFrame.nHeight); //Set correct client size
-//ClipOrCenterWindowToMonitor(hWnd, MONITOR_CENTER | MONITOR_WORKAREA, _nTitleOffset);
-  ShowWindow( hwnd, SW_SHOWDEFAULT );
-  return hwnd;
+	#ifndef USE_Transparent_PixView
+		GetClientRect( hwnd, &rcClient );
+		GetWindowRect( hwnd, &rcWindow );
+		ptDiff.x = (rcWindow.right - rcWindow.left) - rcClient.right;
+		ptDiff.y = (rcWindow.bottom - rcWindow.top) - rcClient.bottom;
+		MoveWindow( hwnd, rcWindow.left, rcWindow.top, _context->mem_width + ptDiff.x, _context->mem_height + ptDiff.y, false);
+	#endif
+	
+	//int _nTitleOffset = fClientResize(hWnd, _oWindow->vFrame.nWidth, _oWindow->vFrame.nHeight); //Set correct client size
+	//ClipOrCenterWindowToMonitor(hWnd, MONITOR_CENTER | MONITOR_WORKAREA, _nTitleOffset);
+	
+
+	ShowWindow( hwnd, SW_SHOWDEFAULT );
+	return hwnd;
+}
+
+
+static void draw_square(uint32_t _color, uint32_t* _pix, int _lsize, int _posx, int _posy, int _width, int _height, int inc){
+	int y = _height-1;
+	
+	for(int x = 0; x <  _width;  x+=inc)	{_pix[_posx+x+((_posy+0) *_lsize)]			= _color;}
+	for(int x = 0; x <  _width;  x+=inc) 	{_pix[_posx+x+((_posy+y) * _lsize)] 		= _color;}
+	for(y = 0; y 	 <  _height; y+=inc)	{_pix[_posx+0+((_posy+y) * _lsize)] 		= _color;}
+	for(y = 0; y 	 <  _height; y+=inc)	{_pix[_posx+_width-1+((_posy+y) * _lsize)] 	= _color;}
 }
 
 void pixView_MakeSurface(ContextInf* _context){
@@ -185,7 +198,30 @@ void pixView_MakeSurface(ContextInf* _context){
 	}
 	_context->hbmp = CreateDIBSection( hdc, &bmi, DIB_RGB_COLORS, (void**)&_context->pixels, NULL, 0 );
 	//DeleteDC( hdc );
+///////////////
 	ReleaseDC(_hwnd,  hdc );
+	
+	#ifndef USE_Transparent_PixView
+	return;
+	#endif
+	//Clear with background color//
+	_memset(_context->pixels, 0xFF, _context->mem_width*_context->mem_height*4 );
+
+	//black border
+	draw_square(0xFF555555,  _context->pixels,_context->mem_width, 0,0, _context->mem_width, _context->mem_height,1);
+	draw_square(0xFFCCCCCC,  _context->pixels,_context->mem_width, 2,2, _context->mem_width-2, 1,3);
+	draw_square(0xFFAAAAAA,  _context->pixels,_context->mem_width, _context->off_x-1,_context->off_y-1, _context->width+2, _context->height+2,1);
+	
+	//Minimise
+	draw_square(0xFF000000,  _context->pixels,_context->mem_width, _context->mem_width-90,5, 10, 3,1);
+	
+	//Maximise
+	draw_square(0xFF000000,  _context->pixels,_context->mem_width, _context->mem_width-55,2, 10, 10,2);
+	draw_square(0xFF000000,  _context->pixels,_context->mem_width, _context->mem_width-60,5, 10, 10,1);
+	
+	//Close
+	draw_square(0xFFAA0000,  _context->pixels,_context->mem_width, _context->mem_width-25,5, 10, 3,1);
+	draw_square(0xFFAA0000,  _context->pixels,_context->mem_width, _context->mem_width-25,10, 10, 3,2);
 }
 
 void pixView_update(ContextInf* _context){
@@ -206,8 +242,8 @@ void pixView_update(ContextInf* _context){
 	HBITMAP hbmOld = (HBITMAP)SelectObject( hdcMem, _context->hbmp );
 
 	SIZE frameSize;
-	frameSize.cx = _context->width  + WIN_BORDER_L + WIN_BORDER_R;
-    frameSize.cy = _context->height + WIN_BORDER_T + WIN_BORDER_B;
+	frameSize.cx = _context->mem_width;
+    frameSize.cy = _context->mem_height;
 	POINT ptSrc = {0,0};
 	int nWinAlpha = 255;
 	BLENDFUNCTION bf = {AC_SRC_OVER, 0, nWinAlpha, AC_SRC_ALPHA};
