d76cf29553bdfed56f593ae8a3328ab2e336529f
Maeiky <maeiky@gmail.com>
2021-07-07 17:12:08 -0400

    fix

--- src/XE/Module/PE/FT_Sys.h
+++ src/XE/Module/PE/FT_Sys.h
@@ -47,7 +47,7 @@ DWORD WINAPI sys_GetLastError(VOID){
 		DWORD bufLen = FormatMessage(	FORMAT_MESSAGE_ALLOCATE_BUFFER |
 										FORMAT_MESSAGE_FROM_SYSTEM |
 										FORMAT_MESSAGE_IGNORE_INSERTS,
-										NULL,error,MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),(LPTSTR) &lpMsgBuf,0, NULL );
+										NULL,error,MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),(LPSTR) &lpMsgBuf,0, NULL );
 		if (bufLen){
 		  LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
 		//  std::string result(lpMsgStr, lpMsgStr+bufLen); //TODO
